d8f2a8234c450ffc7f6d524d2f6366649edcca03
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 02:05:47 -0400

    He terminado el servicio de DetallePagos

---
+++ SwiftPay/SwiftPay/Services/DetallePagosService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwiftPay.DAL;
+using SwiftPay.Models;
+using System.Linq.Expressions;
+
+namespace SwiftPay.Services
+{
+	public class DetallePagosService
+	{
+		private readonly Context _context;
+
+		public DetallePagosService(Context Context)
+		{
+			_context = Context;
+		}
+
+		public async Task<bool> Insertar(DetallePagos detallePago)
+		{
+			_context.Add(detallePago);
+			var guardado = await _context.SaveChangesAsync() > 0;
+			_context.DetallePagos!.Entry(detallePago).State = EntityState.Detached;
+			return guardado;
+		}
+
+		public async Task<DetallePagos?> Buscar(int detallePagoId)
+		{
+			return await _context.DetallePagos!.AsNoTracking().FirstOrDefaultAsync(d => d.DetallePagoId == detallePagoId);
+		}
+
+		public async Task<List<DetallePagos>> Listar(Expression<Func<DetallePagos, bool>> criterio)
+		{
+			return await _context.DetallePagos!.AsNoTracking().Where(criterio).ToListAsync();
+		}
+	}
+}
