0d763de84940dee03dd371e0378a362a56299170
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 01:52:32 -0400

    Agregamos el modelo DetalleRegante

---
+++ SwiftPay/SwiftPay/Models/DetalleRegante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftPay.Models
+{
+    public class DetalleRegante
+    {
+        [Key]
+        public int DetalleReganteId { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [ForeignKey("Regante")]
+        public int ReganteId { get; set; }
+
+        [Required(ErrorMessage = "El campo Código Parcela es obligatorio")]
+        public string? CodigoParcela { get; set; }
+
+        [Required(ErrorMessage = "El campo Tareas es obligatorio")]
+        public float Tareas { get; set; }
+
+        [Required(ErrorMessage = "El campo Tipo Irrigación es obligatorio")]
+        [RegularExpression("^(Bomba|Gravedad)$", ErrorMessage = "El tipo de irrigación solo puede ser 'bomba' o 'gravedad'")]
+        public string? TipoIrrigacion { get; set; }
+    }
+}
