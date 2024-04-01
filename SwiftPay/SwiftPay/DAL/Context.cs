2383e03fd5df15429b0270bdcb8d1ec3e9d4587c
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 03:12:43 -0400

    Agregamos un OnModelCreating para crear el usuario por defecto para acceder

--- SwiftPay/SwiftPay/DAL/Context.cs
+++ SwiftPay/SwiftPay/DAL/Context.cs
@@ -17,5 +17,27 @@ namespace SwiftPay.DAL
 		public DbSet<DetallePagos> DetallePagos { get; set; }
 
 		public Context(DbContextOptions<Context> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// Crear usuario por defecto
+			modelBuilder.Entity<Usuarios>().HasData(
+				new Usuarios
+				{
+					UsuarioId = 1,
+					Nombre = "Administrador",
+					Apellido = "del Sistema",
+					Cedula = "000-0000000-0",
+					Usuario = "administrador",
+					Contrasena = "12345",
+					Telefono = "8090000000",
+					CorreoElectronico = "administrador-sistema@gmail.com",
+					Direccion = "Ninguna",
+					Rol = "Administrador"
+
+				});
+		}
 	}
 }
