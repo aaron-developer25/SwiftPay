00cd2fffc3b6ddcc77d625bea502a7e23dcfb016
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 01:52:23 -0400

    Agregamos el modelo DetallePagos

---
+++ SwiftPay/SwiftPay/Models/DetallePagos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SwiftPay.Models
+{
+	public class DetallePagos
+	{
+        [Key]
+        public int DetallePagoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [ForeignKey("Pago")]
+        public int PagoId { get; set; }
+
+        [Required(ErrorMessage = "El campo Código Parcela es obligatorio")]
+        public string? CodigoParcela { get; set; }
+
+        [Required(ErrorMessage = "El campo Tareas es obligatorio")]
+        public float Tareas { get; set; }
+
+        [RegularExpression("^(Bomba|Gravedad)$", ErrorMessage = "El tipo de irrigación solo puede ser 'bomba' o 'gravedad'")]
+        public string? TipoIrrigacion { get; set; }
+
+        [Required(ErrorMessage = "El campo Periodos es obligatorio")]
+        public int Periodos { get; set; }
+    }
+}
