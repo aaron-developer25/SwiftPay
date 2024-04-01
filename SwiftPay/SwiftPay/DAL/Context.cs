0d16b56e5c3cb1f33671d440c29e3d6ea6c1eeda
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 01:53:44 -0400

    Agregamos el contexto

---
+++ SwiftPay/SwiftPay/DAL/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwiftPay.Models;
+
+namespace SwiftPay.DAL
+{
+	public class Context : DbContext
+	{
+		public DbSet<Empresa> Empresas { get; set; }
+		public DbSet<Asociaciones> Asociaciones { get; set; }
+		public DbSet<Bloques> Bloques { get; set; }
+		public DbSet<SalidaCaja> SalidaCaja { get; set; }
+		public DbSet<Regantes> Regantes { get; set; }
+		public DbSet<DetalleRegante> DetalleRegantes { get; set; }
+		public DbSet<Parametros> Parametros { get; set; }
+		public DbSet<Usuarios> Usuarios { get; set; }
+		public DbSet<Pagos> Pagos { get; set; }
+		public DbSet<DetallePagos> DetallePagos { get; set; }
+
+		public Context(DbContextOptions<Context> options) : base(options) { }
+	}
+}
