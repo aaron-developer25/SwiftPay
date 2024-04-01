25f5d7e041cf112b84375b4584727a1583e962ba
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 02:05:53 -0400

    He terminado el servicio de Pagos

---
+++ SwiftPay/SwiftPay/Services/PagosService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SwiftPay.DAL;
+using SwiftPay.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SwiftPay.Services
+{
+    public class PagosService
+	{
+        private readonly Context _context;
+
+        public PagosService(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Verificar(int PagoId)
+        {
+            return await _context.Pagos.AnyAsync(a => a.PagoId == PagoId);
+        }
+
+		public async Task<int> Insertar(Pagos Pago)
+		{
+			_context.Add(Pago);
+			await _context.SaveChangesAsync();
+			return Pago.PagoId;
+		}
+
+		public async Task<bool> Modificar(Pagos pago)
+		{
+			if (await Verificar(pago.PagoId))
+			{
+				_context.Update(pago);
+				var guardado = await _context.SaveChangesAsync() > 0;
+				_context.Pagos!.Entry(pago).State = EntityState.Detached;
+				return guardado;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public async Task<Pagos?> Buscar(int PagoId)
+        {
+            return await _context.Pagos
+				.AsNoTracking()
+                .FirstOrDefaultAsync(a => a.PagoId == PagoId);
+        }
+
+        public async Task<List<Pagos>> Listar(Expression<Func<Pagos, bool>> criterio)
+        {
+            return await _context.Pagos
+				.AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
