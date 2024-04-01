5a82a6f8c83063ee1575452d04687359cc2f8e5b
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-04-01 01:52:14 -0400

    Agregamos el modelo Asociacion

---
+++ SwiftPay/SwiftPay/Models/Asociaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftPay.Models
+{
+    public class Asociaciones
+    {
+        [Key]
+        public int AsociacionId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [RegularExpression(@"^[A-ZÁÉÍÓÚÑ][a-zA-ZÁÉÍÓÚÑ ]*$", ErrorMessage = "El nombre debe comenzar con una letra mayúscula y no debe contener números ni símbolos.")]
+        public string? Nombre { get; set; }
+    }
+}
